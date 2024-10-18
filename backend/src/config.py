d1fb37fd2bbd5ec6778176336c39f17c8a9266ad
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2024-10-17 22:35:39 -0500

    Feat: added SSL_CERT path for connectivity

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,7 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str 
+    SSL_CERT: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
