c7f1e6d604a24fb7bc6e9810f4217958f56573e5
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2024-10-24 11:03:55 -0500

    Fix: added missing required attributes.

--- backend/src/schemas/vendors.py
+++ backend/src/schemas/vendors.py
@@ -9,36 +9,47 @@ class VendorType(str, Enum):
     equipment = "Equipment"
 
 class Vendor(BaseModel):
-    vendor_id: int
+    vendor_id: str
     company_name: str
+    vendor_contact: str 
     email: EmailStr
     phone_number: str
-    address_line1: Optional[str]
-    address_line2: Optional[str]
-    city: Optional[str]
-    state: Optional[str]
-    zip_code: Optional[str]
+    address_line1: str
+    address_line2: str
+    city: str
+    state: str
+    zip_code: str
     country: str
-    contract_start: Optional[date | None] = None
-    contract_end: Optional[date | None] = None
+    contract_start_date: date
+    contract_end_date: date
     vendor_type: VendorType
 
 class VendorCreateModel(BaseModel):
-    vendor_id: int
     company_name: str
+    vendor_contact: str 
     email: EmailStr
     phone_number: str
+    address_line1: str
+    address_line2: Optional[str | None] = None
+    city: str
+    state: str
+    zip_code: str
     country: str
+    contract_start_date: date
+    contract_end_date: date
+    vendor_type: VendorType
 
 class VendorUpdateModel(BaseModel):
+    company_name: str
+    vendor_contact: str
     email: EmailStr
-    phone_number: Optional[str]
-    address_line1: Optional[str]
-    address_line2: Optional[str]
-    city: Optional[str]
-    state: Optional[str]
-    zip_code: Optional[str]
-    contract_start: Optional[date] = None
-    contract_end: Optional[date] = None
+    phone_number: str
+    address_line1: str
+    address_line2: Optional[str | None] = None
+    city: str
+    state: str
+    zip_code: str
+    contract_start_date: date 
+    contract_end_date: date 
     vendor_type: VendorType
     country: str
