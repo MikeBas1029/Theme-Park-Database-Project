618ce838701396fa460cda2664661970d755758f
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2024-10-25 18:09:07 -0500

    updated sidebar(s), folder setup

---
+++ client/src/scenes/customerscreens/customerdashboard/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../../components/Header";
+
+
+
+const CustomerDashboard = () => {
+    return <Box m="20px"> 
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="DASHBOARD" subtitle="Customer Dashboard " />
+            </Box>
+           </Box>
+    
+    }
+    
+    export default CustomerDashboard; 
