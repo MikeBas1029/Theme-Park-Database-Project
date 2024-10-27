eb6fd95fef91400d7a123c731181a95516244d7b
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2024-10-27 09:05:04 -0500

    Feat: added expiry_at attr to token blocklist table.

--- backend/src/models/token_blocklist.py
+++ backend/src/models/token_blocklist.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 class TokenBlocklist(SQLModel, table=True):
     __tablename__ = "token_blocklist"
     
     jti: str = Field(primary_key=True, nullable=False, max_length=400)
+    expiry_at: datetime = Field(nullable=False)
