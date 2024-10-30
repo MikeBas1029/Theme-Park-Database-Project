e6308b5d3a949031240a2788cbebca672622abdc
Mmokut Umoh <105234838+Yukin3@users.noreply.github.com>
2024-10-29 19:26:05 -0500

    customer ticket pages, update/delete button

--- client/src/scenes/customerscreens/customertickets/purchaseTickets.jsx
+++ client/src/scenes/customerscreens/customertickets/purchaseTickets.jsx
@@ -1,14 +1,31 @@
-import { Box } from "@mui/material";
 import Header from "../../../components/Header";
+import { Box, useTheme, Button} from "@mui/material";
+import { tokens } from "../../../theme";
+import { useNavigate } from "react-router-dom";
 
 
 const PurchaseTickets = () => {
-    return <Box m="20px"> 
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Header title="Purchase a Ticket" subtitle="View available admission tickets" />
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const navigate = useNavigate();
+
+    return (
+        <Box m="20px">
+        {/* Print | Export | Add  */}
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="My Tickets" subtitle="View ticket purchase and activation history" />
+
+        </Box>
+
+        {/*Form fields, missing validation method linkings + user auth */}
+            <Box
+                m="40px 0 0 0"
+                height="75vh"
+            >
+                Hi there
             </Box>
-           </Box>
-    
-    }
+        </Box>
+    );
+};
     
     export default PurchaseTickets; 
